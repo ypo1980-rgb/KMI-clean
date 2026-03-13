899f5176b4ac925807ec8c34bc98097a6616ff03
ypo1980-rgb <ypo1980@gmail.com>
2026-03-13 15:42:30 +0200

    Add iOS project files
    ;;

---
+++ ios/KMI-iOS_REAL/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KMI-iOS
+//
+//  Created by יובל פולק on 08/02/2026.
+//
+
+import SwiftUI
+import Shared
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
